602b4f099a549c03cf2cdc35ac315515cc3755cc
play-baby <playbaby@users.noreply.github.com>
2026-05-10 16:47:52 +0300

    Fix missing auth_views import in urls.py

--- lingerie_crm/urls.py
+++ lingerie_crm/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 from . import views
 from deploy_view import run_deploy
 
