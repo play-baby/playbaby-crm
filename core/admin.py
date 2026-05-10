db555aedee58d0d14cabc574137a0e056cb34d8c
play-baby <elfagersix@gmail.com>
2026-05-10 07:13:16 +0300

    feat: role landing pages, dashboard block control

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import SiteSetting, InvoiceTemplate
+from django.contrib.auth.models import Group
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.contrib import messages
+from .models import SiteSetting, InvoiceTemplate, RoleLanding
 
 @admin.register(SiteSetting)
 class SiteSettingAdmin(admin.ModelAdmin):
@@ -16,3 +20,17 @@ class InvoiceTemplateAdmin(admin.ModelAdmin):
 
     def has_delete_permission(self, request, obj=None):
         return False
+
+
+@admin.register(RoleLanding)
+class RoleLandingAdmin(admin.ModelAdmin):
+    list_display = ('group', 'landing_page', 'dashboard_blocked')
+    list_editable = ('landing_page', 'dashboard_blocked')
+
+    def has_add_permission(self, request):
+        return False if RoleLanding.objects.count() >= Group.objects.count() else True
+
+    def changelist_view(self, request, extra_context=None):
+        for group in Group.objects.all():
+            RoleLanding.objects.get_or_create(group=group)
+        return super().changelist_view(request, extra_context=extra_context)
