4ff4a32f2b8d74bc0bfcddbe434c498d2d27d0d1
play-baby <elfagersix@gmail.com>
2026-05-10 06:33:20 +0300

    fix: force UTF-8 locale to prevent Arabic double-encoding

--- lingerie_crm/wsgi.py
+++ lingerie_crm/wsgi.py
@@ -1,13 +1,17 @@
 """
 WSGI config for lingerie_crm project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+It exposes the WSGI callable as a module-level variable named `application`.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+
+# Force UTF-8 locale to prevent double-encoding of Arabic text
+os.environ.setdefault('LANG', 'en_US.UTF-8')
+os.environ.setdefault('LC_ALL', 'en_US.UTF-8')
 
 from django.core.wsgi import get_wsgi_application
 
